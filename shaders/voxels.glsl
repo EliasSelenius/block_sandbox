39b1183eaa8bf29f27af42ebefdc22b5311c86a2
Elias Sebastian Selenius Haneborg <ESSH@LIVE.NO>
2026-02-13 18:44:22 +0100

    voxel raytracing perf gain

--- shaders/voxels.glsl
+++ shaders/voxels.glsl
@@ -11,6 +11,8 @@
 
 // TODO: put into camera.glsl
 uniform vec3 u_camera_world_pos;
+uniform ivec3 u_center_chunk_coord;
+
 
 uniform sampler2D u_spritesheet;
 
@@ -26,40 +28,32 @@ layout (std430) readonly buffer Textures {
 #define Chunk_Pool_Size (Render_Radius*2 + 1)
 #define Pool_Size (Chunk_Pool_Size*Chunk_Pool_Size)
 #define Chunk_Size 32
-
-struct Chunk {
-    ivec4 coord;
-    uint16_t blocks[Chunk_Size*Chunk_Size*Chunk_Size];
-};
+#define Chunk_Block_Count (Chunk_Size * Chunk_Size * Chunk_Size)
 
 layout (std430) readonly buffer VoxelData {
-    Chunk chunks[];
+    uint16_t blocks[];
 };
 
-int get_chunk_index(ivec3 chunk_coord) {
-    for (int i = 0; i < Pool_Size; i++) {
-        if (chunks[i].coord.xyz == chunk_coord) return i;
-    }
+int get_index(ivec3 v) { return v.z * Chunk_Size * Chunk_Size + v.y * Chunk_Size + v.x; }
+ivec3 chunk_coord(ivec3 block_coord) { return ivec3(floor(vec3(block_coord) / float(Chunk_Size))); }
 
-    return -1;
-}
+int get_block(ivec3 block_coord) {
+    int cpsh = Chunk_Pool_Size/2;
+    ivec3 c = chunk_coord(block_coord) + ivec3(cpsh, 0, cpsh) - u_center_chunk_coord;
+    int chunk_index = c.x + c.z * Chunk_Pool_Size;
 
-int get_index(ivec3 v) {
-    return v.z * Chunk_Size * Chunk_Size + v.y * Chunk_Size + v.x;
-}
+    ivec3 chunk_pool_dim = ivec3(Chunk_Pool_Size, 1, Chunk_Pool_Size);
 
-ivec3 chunk_coord(ivec3 block_coord) {
-    return ivec3(floor(vec3(block_coord) / float(Chunk_Size)));
-}
+    // negative block_id represent out-of-bounds (no block data)
+    if (c.x < 0 || c.x >= chunk_pool_dim.x) return -1;
+    if (c.z < 0 || c.z >= chunk_pool_dim.z) return -2;
+    if (c.y < 0 || c.y >= chunk_pool_dim.y) return -3;
 
-uint16_t get_block(ivec3 coord) {
-    ivec3 chunk_coord = chunk_coord(coord);
-    int chunk = get_chunk_index(chunk_coord);
-    if (chunk == -1) return uint16_t(0);
+    ivec3 local_coord = block_coord - chunk_coord(block_coord) * Chunk_Size;
+    int local_index = get_index(local_coord);
 
-    ivec3 rel = (coord - chunk_coord*Chunk_Size);
-    int index = get_index(rel);
-    return chunks[chunk].blocks[index];
+    int block_index = chunk_index * Chunk_Block_Count + local_index;
+    return int(blocks[block_index]);
 }
 
 
@@ -117,22 +111,34 @@ Voxel_Traversal_State traverse(Voxel_Traversal_State ts) {
     return ts;
 }
 
+vec2 ray_aabb_intersects(vec3 o, vec3 r, vec3 l, vec3 h) {
+    vec3 t_low  = (l - o) / r;
+    vec3 t_high = (h - o) / r;
+    vec3 t_close = min(t_low, t_high);
+    vec3 t_far   = max(t_low, t_high);
+
+    return vec2(max_axis(t_close), min_axis(t_far));
+}
+
 struct HitInfo {
     vec2 uv;
     vec3 hit_pos;
     vec3 normal;
     float dist;
-    uint16_t block_id;
+    int block_id;
 };
 
 HitInfo raycast(vec3 o, vec3 d) {
     Voxel_Traversal_State state = start_traversal(o, d);
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < 1024; i++) {
         ivec3 prev_coord = state.coord;
         state = traverse(state);
 
-        uint16_t block_id = get_block(state.coord);
-        if (block_id != uint16_t(0)) {
+        int block_id = get_block(state.coord);
+
+        if (block_id < 0) break;
+
+        if (block_id != 0) {
             HitInfo hit;
             hit.dist = travel_distance(state).x;
             hit.hit_pos = o + d*hit.dist;
@@ -151,15 +157,8 @@ HitInfo raycast(vec3 o, vec3 d) {
     }
 
     HitInfo hit;
-    hit.block_id = uint16_t(0);
+    hit.block_id = 0;
     return hit;
-}
-
-
-float get_fragdepth_from_world_space_point(vec3 point) {
-    vec4 clip_pos = camera.projection * camera.view * vec4(point, 1.0);
-    vec3 clip = clip_pos.xyz / clip_pos.w;
-    return (clip.z + 1.0) * 0.5;
 }
 
 
@@ -194,30 +193,47 @@ void main() {
     vec3 ray_origin = u_camera_world_pos;
     vec3 ray = camera_ray(frag_input.ndc);
 
-    HitInfo hit = raycast(ray_origin, ray);
-    if (hit.block_id == uint16_t(0)) discard;
+    { // clamping ray_origin to world bounding box
+        ivec3 chunk_pool_dim = ivec3(Chunk_Pool_Size, 1, Chunk_Pool_Size);
+        vec3 dim = vec3(chunk_pool_dim * Chunk_Size);
+        vec3 center = u_center_chunk_coord * Chunk_Size + vec3(float(Chunk_Size)/2.0 - 0.5);
+        vec3 l = center - dim*0.5;
+        vec3 h = center + dim*0.5;
 
-    vec3 pos = (camera.view * vec4(hit.hit_pos, 1.0)).xyz;
-    vec3 normal = mat3(camera.view) * hit.normal;
+        vec3 o = ray_origin;
+        vec3 d = ray;
+
+        vec2 inter = ray_aabb_intersects(o, d, l, h);
+        float close = inter.x;
+        float far = inter.y;
+        if ((close > 0.0) && (close < far)) {
+            ray_origin = o + close*d;
+        }
+    }
+
+    HitInfo hit = raycast(ray_origin, ray);
+    if (hit.block_id == 0) discard;
+
+    vec3 view_pos = (camera.view * vec4(hit.hit_pos, 1.0)).xyz;
+    vec3 view_normal = mat3(camera.view) * hit.normal;
 
     float metallic = 0.1;
     float roughness = 0.1;
 
-    int block_id = int(hit.block_id);
     int ss_size = textureSize(u_spritesheet, 0).x / Block_Pixel_Size;
     vec2 uv_scale = vec2(1.0 / float(ss_size));
-    vec2 uv_offset = vec2(block_id % ss_size, block_id / ss_size) * uv_scale;
+    vec2 uv_offset = vec2(hit.block_id % ss_size, hit.block_id / ss_size) * uv_scale;
 
     vec2 uv = uv_offset + hit.uv * uv_scale;
     vec3 albedo = texture(u_spritesheet, uv).rgb;
 
-    normal = mat3(camera.view) * normal_from_sampler(u_spritesheet, hit.uv, uv_offset, uv_scale, hit.normal);
+    view_normal = mat3(camera.view) * normal_from_sampler(u_spritesheet, hit.uv, uv_offset, uv_scale, hit.normal);
 
-    FragPos_Metallic = vec4(pos, metallic);
-    FragNormal_Roughness = vec4(normal, roughness);
-    FragColor = vec3(albedo);
+    FragPos_Metallic     = vec4(view_pos,    metallic);
+    FragNormal_Roughness = vec4(view_normal, roughness);
+    FragColor            = vec3(albedo);
 
-    gl_FragDepth = get_fragdepth_from_world_space_point(hit.hit_pos);
+    gl_FragDepth = get_fragdepth_from_view_space_point(view_pos);
 }
 #endif
 
